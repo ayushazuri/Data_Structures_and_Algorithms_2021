f4246fcf077be75c2a9b07da664d07899b609e60
Ayush Singh <ayushazuri@gmail.com>
2021-07-02 16:49:26 +0530

    Rain Water trapping

--- Stack/src/RainTrapping.java
+++ Stack/src/RainTrapping.java
@@ -1,3 +1,12 @@
+//For all the blocks, we need to find the total water that would be trapped in the empty areas which is enclosed by the blocks. The trick is simple =>
+//    1. For each block find the greatest block on the left and the greatest block on the right,
+//        because this will be the total area in which the water be trapped, and this will also give the level of the water.
+//    2. To get the max level of the water, just take the minimum of the two greatest blocks on the left and right. That would be the level.
+//    3. Now calculate the area of water in a particular vertical block for all the element,
+//        by subtracting the level of water from the height of the block, above which you want to calc the water.
+//    4. Add all the area of water found above each block.
+
+
 import java.util.Arrays;
 
 public class RainTrapping {
